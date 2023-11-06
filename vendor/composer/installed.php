57b1c700422a4f963af175464116b3a39afc1c79
Martin Ronot <ronot.martin@gmail.com>
2023-11-06 15:17:34 +0100

    Ajout Composer/Autoloader

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 's3/touiteur',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f4d63ef1e963d4c776bb344e9de7e5b935136e46',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        's3/touiteur' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f4d63ef1e963d4c776bb344e9de7e5b935136e46',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
