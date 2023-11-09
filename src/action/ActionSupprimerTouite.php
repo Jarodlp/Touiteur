2d789e51b1c8307804ac7cf54706559a38a6096e
TibereLN <lenaline2u@etu.univ-lorraine.fr>
2023-11-09 08:19:37 +0100

    modif suppression touite

--- src/action/ActionSupprimerTouite.php
+++ src/action/ActionSupprimerTouite.php
@@ -7,13 +7,17 @@ use \iutnc\touiteur\db\ConnectionFactory;
 class ActionSupprimerTouite extends Action {
     public function execute() : string {
         $aff="";
+        $idTouite = $_GET["id"];
         $connexion = ConnectionFactory::makeConnection();
         $query = "DELETE FROM touiteimage WHERE idTouite = ?;
         DELETE FROM touitenote WHERE idTouite = ?;
         DELETE FROM touitetag WHERE idTouite = ?;
         DELETE FROM touite WHERE idTouite = ?;";
         $statment = $connexion->prepare($query);
-        $statment->bindParam(1, $_GET["id"]);
+        $statment->bindParam(1, $idTouite);
+        $statment->bindParam(2, $idTouite);
+        $statment->bindParam(3, $idTouite);
+        $statment->bindParam(4, $idTouite);
         $statment->execute();
         $result = $statment->fetch();
         $aff.="Votre touite à bien été supprimé";
